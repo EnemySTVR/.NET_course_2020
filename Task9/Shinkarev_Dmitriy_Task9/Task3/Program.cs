50b1342b19f0fe9f55425b99152ef130181b85b0
EnemySTVR <shinkarev_dmitriy@mail.ru>
2020-09-22 21:29:59 +0300

    Add task 9 / 3

---
+++ Task9/Shinkarev_Dmitriy_Task9/Task3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Enter text on english:");
+            string input = Console.ReadLine();
+
+            var regex = new Regex("[A-z]+");
+            var matches = regex.Matches(input);
+
+            var wordCounter = new Dictionary<string, int>();
+
+            foreach (var m in matches)
+            {
+                // ContainsKey чувствителен к регистру.
+                // Я напишу свой ContainsKey с игнором регистра ̶и̶ ̶к̶у̶р̶т̶и̶з̶а̶н̶к̶а̶м̶и̶.
+                string overlapKey;
+                if (!ContainsKeyWithIgnoreRegister(wordCounter, m.ToString(), out overlapKey))
+                {
+                    wordCounter.Add(m.ToString(), 1);
+                }
+                else
+                {
+                    wordCounter[overlapKey]++;
+                }
+            }
+
+            WriteDictionary(wordCounter);
+
+            Console.ReadLine();
+        }
+
+        static bool ContainsKeyWithIgnoreRegister(Dictionary<string, int> dictionary, string searchKey, out string outKey)
+        {
+            foreach (var pair in dictionary)
+            {
+                if (pair.Key.ToLower().Equals(searchKey.ToLower()))
+                {
+                    outKey = pair.Key;
+                    return true;
+                }
+            }
+
+            outKey = "";
+            return false;
+        }
+
+        static void WriteDictionary(Dictionary<string, int> pairs)
+        {
+            foreach (var p in pairs)
+            {
+                Console.WriteLine($"Слово {p.Key} встречается {p.Value} раз(а).");
+            }
+        }
+    }
+}
