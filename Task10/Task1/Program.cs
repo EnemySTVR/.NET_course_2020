11d133fb52e029a8be9f3b5f20b9a4d613289952
EnemySTVR <shinkarev_dmitriy@mail.ru>
2020-09-24 13:26:18 +0300

    Refactoring

--- Task10/Task1/Program.cs
+++ Task10/Task1/Program.cs
@@ -15,7 +15,7 @@ namespace Task1
             AlphavitSortInLengthSortDelegate myDelegate = null;
             myDelegate += AlphavitSortInLengthSort;
 
-            LenghtAndAlphavitSort(array, myDelegate);
+            LenghtAndAlphavitSort(ref array, myDelegate);
 
             // Профита в этом действии я не увидел, но, говорят, хороший тон.
             myDelegate -= AlphavitSortInLengthSort;
@@ -33,34 +33,34 @@ namespace Task1
             }
         }
 
-        private static void LenghtAndAlphavitSort(string[] array, AlphavitSortInLengthSortDelegate myDelegate)
+        private static void LenghtAndAlphavitSort(ref string[] array, AlphavitSortInLengthSortDelegate myDelegate)
         {
             array = array.OrderBy(x => x.Length).ToArray();
-                     
-            // цикл ищет элементы в массиве с одинаковой длинной и сортирует их методом переданным делегатом
-            //for (int i = 0; i < array.Length; i++)
-            //{
-            //    int previousLength = 0;
-            //    int overlapLength = 0;
-            //    bool lastElement = i == array.Length - 1;
 
-            //    if (previousLength == array[i].Length)
-            //    {
-            //        overlapLength++;
-            //        if (lastElement)
-            //        {
-            //            overlapLength++;
-            //            myDelegate(array, overlapLength, i + 1);
-            //        }
-            //    }
-            //    else if (overlapLength > 0)
-            //    {
-            //        overlapLength++;
-            //        myDelegate(array, overlapLength, i);
-            //        overlapLength = 0;
-            //    }
-            //    previousLength = array[i].Length;
-            //}
+            // цикл ищет элементы в массиве с одинаковой длинной и сортирует их методом переданным делегатом
+            for (int i = 0; i < array.Length; i++)
+            {
+                int previousLength = 0;
+                int overlapLength = 0;
+                bool lastElement = i == array.Length - 1;
+
+                if (previousLength == array[i].Length)
+                {
+                    overlapLength++;
+                    if (lastElement)
+                    {
+                        overlapLength++;
+                        myDelegate(array, overlapLength, i + 1);
+                    }
+                }
+                else if (overlapLength > 0)
+                {
+                    overlapLength++;
+                    myDelegate(array, overlapLength, i);
+                    overlapLength = 0;
+                }
+                previousLength = array[i].Length;
+            }
         }
 
 
