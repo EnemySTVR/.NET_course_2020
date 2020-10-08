ea97d1819b9d7ee07142fc1f56d968ed1098769c
EnemySTVR <shinkarev_dmitriy@mail.ru>
2020-10-08 20:39:42 +0300

    add second forms

---
+++ Task14/Shinkarev_Dmitriy_Task14/Task/SecondForms/PrizeForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Task.SecondForms
+{
+    public partial class PrizeForm : Form
+    {
+        Prize _changedPrize;
+
+        public PrizeForm()
+        {
+            InitializeComponent();
+            Text = "Add prize";
+            newPrizeAcceptButton.Text = "Add";
+            newPrizeAcceptButton.Click += AddPrizeButtonClick;
+        }
+
+        public PrizeForm(Prize changedPrize)
+        {
+            InitializeComponent();
+            Text = "Change prize";
+            newPrizeAcceptButton.Text = "Change";
+            newPrizeAcceptButton.Click += ChangePrizeButtonClick;
+            _changedPrize = changedPrize;
+            newPrizeNameTextBox.Text = changedPrize.Name;
+            newPrizeDescriptionTextBox.Text = changedPrize.Description;
+        }
+
+        private void AddPrizeButtonClick(object sender, EventArgs e)
+        {
+            var prize = new Prize(newPrizeNameTextBox.Text, newPrizeDescriptionTextBox.Text);
+            DataController.Prizes.Add(prize);
+        }
+
+        private void ChangePrizeButtonClick(object sender, EventArgs e)
+        {
+            int index = DataController.Prizes.IndexOf(_changedPrize);
+            DataController.Prizes[index].ChangeName(newPrizeNameTextBox.Text);
+            DataController.Prizes[index].ChangeDescription(newPrizeDescriptionTextBox.Text);
+
+        }
+    }
+}
